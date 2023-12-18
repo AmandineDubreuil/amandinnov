05e51af80a68b785a40a6537e0f4fff849d58b92
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-12-18 09:56:55 +0100

    first commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amand'Innov</title>
+    <link href="./assets/css/output.css" rel="stylesheet">
+
+</head>
+<body>
+    <h1 class="text-primary font-mono font-bold">c'est un titre</h1>
+</body>
+</html>
