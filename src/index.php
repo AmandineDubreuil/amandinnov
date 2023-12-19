ba9e214805d9073fe0c1c6cc1cfb027106ddb464
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-12-19 12:58:53 +0100

    texte index

--- src/index.php
+++ src/index.php
@@ -16,7 +16,61 @@
 <body>
     <?php require_once './partials/header.php' ?>
 
-    <h1 class="text-primary font-mono font-bold">c'est un titre</h1>
+    <section class="my-8">
+        <h1 class="font-bold">Je conçois et développe votre site Internet</h1>
+        <p>Du design à l'hébergement, je donne vie à la présence en ligne de votre entreprise, de manière unique et sur-mesure.</p>
+        <div><a href=""><i class="fa-solid fa-circle-chevron-down"></i></a></div>
+    </section>
+    <section>
+        <h2>Les services proposés :</h2>
+        <div>
+            <h3>Conception sur Mesure</h3>
+            <p>Je crois en la singularité de chaque entreprise. C’est pourquoi je crée des sites Internet entièrement sur-mesure, reflétant l’identité de votre marque, et répondant à vos objectifs commerciaux uniques.</p>
+        </div>
+        <div>
+            <h3>Responsive Design</h3>
+            <p>Les temps ont changé, et votre site doit s’adapter. Tous nos sites internet sont conçus avec une approche “responsive”, garantissant une expérience fluide et attrayante, quel que soit le dispositif utilisé par vos visiteurs</p>
+        </div>
+        <div>
+            <h3>Optimisation de la Prise de Contact</h3>
+            <p>Faciliter la prise de contact avec vos clients est essentiel. J’intègre des fonctionnalités intuitives et efficaces pour encourager les interactions et les conversions, renforçant ainsi la croissance de votre entreprise.</p>
+        </div>
+        <div>
+            <h3>Comportements Utilisateur</h3>
+            <p>Je mets l’Utilisateur au coeur du processus de conception. En analysant les comportmeents des utilisateurs, je conçois des interfaces conviviales qui maximisent l’engagement et améliorent l’expérience utilisateur.</p>
+        </div>
+        <div>
+            <h3>Écoute et Adaptabilité</h3>
+            <p>L’écoute de mes clients est essentielle à mes yeux. Chaque projet commence par une écoute attentive de vos besoins, objectifs et contraintes. Je m’adapte à votre vision pour créer une solution digitale qui dépasse vos attentes.</p>
+        </div>
+    </section>
+    <section>
+        <h2>Déroulement d'un projet</h2>
+        <div>
+            <p>01</p>
+            <p>Un premier rendez-vous pour faire connaissance et élaborer le <span>cahier des charges</span>.Il s’agit de définir votre projet, lister vos besoins, détailler les fonctionnalités souhaitées. Cette étape est primordiale car elle permet de mettre au clair toutes les informations nécessaires pour la mise en route du projet. Suite à ce rendez-vous , un <span>devis</span> sera établi sur la base de nos échanges.</p>
+        </div>
+        <div>
+            <p>02</p>
+            <p>A cette étape, je vous réalise un <span>wireframe</span> qui représente la trame en noir et blanc de votre site Internet. Il vous aide à visualiser la structure de votre site, et l’organisation des différents composants à l’intérieur de celui-ci.</p>
+        </div>
+        <div>
+            <p>03</p>
+            <p>Ensuite, je définis la <span>charte graphique</span> (couleurs, typologies...) et crée une <span>maquette</span> en couleurs de votre site, avec laquelle vous pourrez intéragir. </p>
+        </div>
+        <div>
+            <p>04</p>
+            <p>Une fois la maquette validée, je passe à l’ <span>intégration</span>, c’est à dire le développement pur de votre projet à l’aide des langages HTML et CSS, et son animation grâce à JavaScript.</p>
+        </div>
+        <div>
+            <p>05</p>
+            <p>Si le projet le nécessite, je m’occupe du <span>développement des fonctionnalités</span> du site. Lors de cette étape, j’utilise des langages de programmation tels que PHP ou JavaScript, ainsi que mon framework de prédilection : Symfony.</p>
+        </div>
+        <div>
+            <p>06</p>
+            <p>Une fois le projet finalisé, je vous <span>forme à l’utilisation de l’interface d’administration</span> et je vous remets  les codes d’accès. Si vous le souhaitez, je peux m’occuper de sa mise en ligne. <span>Votre projet vous appartient</span>, vous êtes libre de choisir votre hébergeur, ainsi que le professionnel que vous désirez pour son évolution future.</p>
+        </div>
+    </section>
 </body>
 
 </html>
