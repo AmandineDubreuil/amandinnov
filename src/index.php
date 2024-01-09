da806dade06cd66bd76a7b3ae4fa434cb14f5e7f
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2024-01-09 15:57:19 +0100

    ajout footer

--- src/index.php
+++ src/index.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amand'Innov</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="./assets/css/output.css" rel="stylesheet">
     <link href="./assets/css/styles.css" rel="stylesheet">
     <link href="./assets/css/index.css" rel="stylesheet">
 
@@ -84,6 +83,8 @@
             <p class="deroulementText">Une fois le projet finalisé, je vous <span class="bold">forme à l’utilisation de l’interface d’administration</span> et je vous remets les codes d’accès. Si vous le souhaitez, je peux m’occuper de sa mise en ligne. <span class="bold">Votre projet vous appartient</span>, vous êtes libre de choisir votre hébergeur, ainsi que le professionnel que vous désirez pour son évolution future.</p>
         </div>
     </section>
+    <?php require_once './partials/footer.php' ?>
+
 </body>
 
 </html>
---
+++ src/partials/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="dfparent">
+        <p class="laius">Développeuse web à Rouen, je vous accompagne pour la création ou la refonte de votre site Internet.</p>
+            <div class="df">
+                <p>réassurance</p>
+                <p><i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-square-instagram"></i></p>
+            </div>
+    </div>
+    <p class="df mentionsContact">
+        <a href="#">Mentions légales</a> &nbsp; - &nbsp; <a href="#">Contact</a>
+    </p>
+</footer>
